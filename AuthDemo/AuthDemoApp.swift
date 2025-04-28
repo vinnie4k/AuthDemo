01db7a8556cdd45bf87d4fe76cee1cc8c11e25e9
Vin Bui <b.vinhan01@gmail.com>
2025-04-28 18:56:24 -0400

    Starter code

---
+++ AuthDemo/AuthDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthDemoApp.swift
+//  AuthDemo
+//
+//  Created by Vin Bui on 4/28/25.
+//
+
+import SwiftUI
+
+@main
+struct AuthDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
